ce5b61047883a6a2ee3a63a3e9586d398f368ce7
Apprentice <apprentice@devbootcamp.com>
2013-03-20 17:39:26 -0700

    added vote, commenting

--- app/controllers/comment.rb
+++ app/controllers/comment.rb
@@ -3,6 +3,20 @@ get '/comment/all' do
   erb :comment_list
 end
 
+post '/comment/new/:this_post_id' do
+  params[:user_id] = get_current_user.id
+  params[:post_id] = params[:this_post_id]
+  @comment = Comment.new( :content => params[:content],
+                          :user_id => params[:user_id],
+                          :post_id => params[:post_id])
+  if @comment.save
+    redirect "/post/#{params[:post_id]}"
+  else
+    @post = Post.find(params[:post_id])
+    erb :post
+  end
+end
+
 get '/comment/:id' do
   @comment = Comment.find(params[:id])
   erb :comment
---
+++ app/controllers/vote.rb
@@ -0,0 +1,12 @@
+post '/vote/post/:this_post_id/new' do
+  PostVote.create(:user_id => get_current_user.id,
+                  :post_id => params[:this_post_id])
+  redirect "/post/#{params[:this_post_id]}"
+end
+
+post '/vote/comment/:this_comment_id/new' do
+  vote = CommentVote.create( :user_id => get_current_user.id,
+                             :post_id => params[:this_comment_id])
+  redirect "/post/#{vote.post.id}"
+end
+
